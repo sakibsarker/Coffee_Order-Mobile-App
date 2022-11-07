5b3bd32e054fa24fc35b82f76c1ca0f7c930c34b
sakibsarker <sakib15-3824@diu.edu.bd>
2022-11-08 00:05:31 +0600

    tab menu added

--- src/History.js
+++ src/History.js
@@ -1,7 +1,7 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-const Shop = () => {
+const History = () => {
   return (
     <View>
       <Text>Shop</Text>
@@ -9,4 +9,4 @@ const Shop = () => {
   )
 }
 
-export default Shop
+export default History

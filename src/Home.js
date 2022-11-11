00b83210782e2d893b6ef5e78ba1b842ba653d87
sakibsarker <sakib15-3824@diu.edu.bd>
2022-11-12 02:52:08 +0600

    alert model icona added homescreen

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,64 @@
-import { View, Text,Image,TouchableOpacity,TextInput } from 'react-native'
-import React from 'react'
+import { View, Text,Image,TouchableOpacity,TextInput,StyleSheet,Modal,Alert,Pressable } from 'react-native'
+import React,{useState} from 'react'
 import { FlashList } from '@shopify/flash-list'
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import Entypo from 'react-native-vector-icons/Entypo';
-
+import Profile from './Profile';
 
 const Home = ({navigation}) => {
   const categories=['All','Coffee','Tea','Pizza']
+
+  const ModelD=()=>{
+    const [modalVisible, setModalVisible] = useState(false);
+    return(
+      <View style={styles.centeredView}>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <View>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}
+            >
+              <Text style={styles.textStyle}>Yes</Text>
+            </Pressable>
+            </View>
+            
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}
+            >
+              <Text style={styles.textStyle}>No</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+      <Pressable
+        style={[styles.button, styles.buttonOpen]}
+        onPress={() => setModalVisible(true)}
+      >
+         <Entypo style={{marginLeft:230}} name='sound' size={25} color={'red'}/>
+      </Pressable>
+    </View>
+    )
+  }
+
   const Header=()=>{
     return(
       <View style={{backgroundColor:'#f3f3f3',borderBottomLeftRadius:30,borderBottomRightRadius:30,marginTop:20}}>
           <View style={{marginTop:20,marginLeft:30,marginRight:30,marginBottom:10,}}>
             <View style={{flexDirection:'row',justifyContent:'space-between'}}>
             <Image style={{height:50,width:50, borderRadius:50,marginBottom:10}} source={require('../assets/sakib.jpg')}/>
-          <Entypo style={{marginTop:10}} name='sound' size={25} color={'red'}/>
+          {/* <Entypo style={{marginTop:10}} name='sound' size={25} color={'red'}/> */}
+          <ModelD/>
             </View>
           
       <Text style={{fontSize:30,marginBottom:10}}>Good Morning,Sakib!</Text>
@@ -90,4 +135,57 @@ const Home = ({navigation}) => {
     //     <Products navigation={navigation}/>
     // </View>
   )}
+
+
+
+  const styles = StyleSheet.create({
+    centeredView: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      marginTop: 0,
+      marginBottom:20,
+    },
+    modalView: {
+      margin: 20,
+      backgroundColor: "white",
+      borderRadius: 20,
+      padding: 35,
+      alignItems: "center",
+      shadowColor: "#000",
+      height:'20%',
+      width:'60%',
+      flexDirection:'row',
+      justifyContent:'space-between',
+      shadowOffset: {
+        width: 0,
+        height: 2
+      },
+      shadowOpacity: 0.25,
+      shadowRadius: 4,
+      elevation: 5
+    },
+    button: {
+      borderRadius: 20,
+      padding: 10,
+   
+    },
+    buttonOpen: {
+      backgroundColor: "#F3f3f3",
+    },
+    buttonClose: {
+      backgroundColor: "#2196F3",
+    },
+    textStyle: {
+      color: "white",
+      fontSize:15,
+      fontWeight:'500',
+      textAlign: "center"
+    },
+    modalText: {
+      marginBottom: 15,
+      textAlign: "center"
+    }
+  });
+  
 export default Home;
